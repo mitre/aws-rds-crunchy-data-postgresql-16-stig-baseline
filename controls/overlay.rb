bf920a176e80adf42770a80d4d4c025781973e6a
George M. Dias <gdias@mitre.org>
2025-05-30 17:16:36 -0500

    Update overlay.rb
    cleaned code for control 'SV-261916'

--- controls/overlay.rb
+++ controls/overlay.rb
@@ -1030,13 +1030,6 @@ include_controls 'crunchy-data-postgresql-16-stig-baseline' do
       describe.one do
         # Case 1: No functions (input) provided → allow either empty output or connection error
         if privilege_escalation_functions.empty?
-          # describe sql_result do
-          #   its('output') { should eq '' }
-          # end
-
-          # describe sql_result do
-          #   it { should match connection_error_regex }
-          # end
           sql_lines = sql_result.output.lines.map(&:strip)
 
           sql_lines.each_with_index do |line, i|
@@ -1055,7 +1048,7 @@ include_controls 'crunchy-data-postgresql-16-stig-baseline' do
       
             if matching_line
               describe "Function '#{function_name}' output line" do
-                it "should end with '|t'" do
+                it "should end with '|t' (Trusted)" do
                   expect(matching_line).to match(/\|t$/)
                 end
               end
